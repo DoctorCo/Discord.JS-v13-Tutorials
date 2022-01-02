16a2f074077b0604713970fab5326c657bac0ab1
DoctorCo <78472859+DoctorCo@users.noreply.github.com>
2022-01-02 14:15:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const Discord = require("discord.js")
 const client = new Discord.Client({ intents: 32767 });
+const token = "" /* insert your discord bot's token in quotes */
 
 client.on("ready", () => {
-    console.log("Bot is online!")
-})
+    console.log("Bot is online!");
+});
 
 client.on("messageCreate", (message) => {
-    const prefix = "-"
+    const prefix = "-";
     if (!message.content.startsWith(prefix)) return;
 
     if (message.content === `${prefix}test`) {
         message.channel.send("Your test has worked!")
     }
-})
+});
 
-client.login("OTI2OTcyMzM0NTI5NzI4NTky.YdDcQg.d6mSxm-lzdBNjFzmigj7xa5fjpQ")
+client.login(token)
